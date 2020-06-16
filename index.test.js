c053d3cd52ee719417727d40b3d767a0f6f52592
chauchakching <chauchakching@gmail.com>
2020-06-16 17:49:21 +0800

    try a test that requires more than 10 MB memory

--- index.test.js
+++ index.test.js
@@ -1,3 +1,10 @@
+const R = require('ramda')
+
 it('should pass', () => {
   expect(true).toBe(true)
+})
+
+it('create a large array that would require memory >10 MB', () => {
+  const a = R.range(4*1000*1000)
+  expect(a).toBeTruthy()
 })
